9704e81fd736fbd1113189696e110c1d73afc167
aidenv <jun90610@gmail.com>
2017-12-17 13:27:04 +0800

    test

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,9 +1,20 @@
 <?php
 
-class IndexController extends Yaf_Controller_Abstract {
-   public function indexAction()
-  {
+declare(strict_type=1);
 
+use Dolpon/library/mysql/mysql as db;
+
+class IndexController extends Yaf_Controller_Abstract {
+  
+    public function __construct()
+    {
+	 $this->db = new db('127.0.0.1', '3306', '')
+
+    }
+
+    public function indexAction()
+   {
+      
 	$this->getView()->assign("content", "hello world");
-  }
+   }
 }
--- application/library/mysql/mysql.php
+++ application/library/mysql/mysql.php
@@ -15,36 +15,51 @@ class mysql
 
     protected $port;
 
+    protected $dbname;
+
     protected $user;
 
     protected $password;
 
     protected $driver;
 
-    public function __construct($host, $prot, $user, $password, $driver)
+    public function __construct($host, $prot, $dbname, $user, $password, $driver)
     {
        	$this->host 	= $host;
         $this->prot 	= $prot;
+	$this->dbname   = $dbname;
         $this->user 	= $user;
         $this->password = $password;
         $this->driver 	= $driver;
-        $this->conn();
+        $db =  $this->conn();
+        return $db;
     }
-
+    
+    /*
+     * 数据库连接方法
+     *
+     *
+     */
     public function conn()
     { 
-		
+        $dsn = 'mysql:host='.$this->host.';dbname='.$this->dbname;
+	if($this->db != null) {
+	    return $this->db;
+	}
+        $this->db = new PDO($dsn, $user, $password);
+        return $this->db;
+        		
     }	
 
-    public function excute()
+    public function excute($sql)
     {
-
+        return $this->db->exec($sql);
 
     }
 
-    public function find()
+    public function find($sql)
     {
-
+	return $this->db->query($sql);
     }
 
     public function findOne()
